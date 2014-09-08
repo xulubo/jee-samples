e146efc2055020f4ea7ea83898cb0f7ea4510df1
Lubo <xulubo@gmail.com>
2014-09-08 13:39:01 -0400

    add

---
+++ TomcatUsingOracleWallet/src/main/java/ca/loobo/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+package ca.loobo.helloworld;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class HelloWorld {
+	
+	@RequestMapping("/")
+	@ResponseBody
+	public String hello() {
+		
+		return "Hello World!";
+
+	}
+	
+
+}
